11f9c32a33d05720b1dd92ede09cbe26c7eb0d2e
Giovanna Anacleto <giovanna18batalha@gmail.com>
2026-08-19 17:15:39 -0300

    Update config.h

--- keyboards/sofle/keymaps/bongo/config.h
+++ keyboards/sofle/keymaps/bongo/config.h
@@ -1,1 +1,7 @@
+#pragma once
 
+// Rotação da tela OLED (se necessário ajustar no Sofle)
+#define OLED_ROTATION_90
+
+// Sincroniza o estado das telas entre o lado mestre e o escravo
+#define SPLIT_OLED_ENABLE
